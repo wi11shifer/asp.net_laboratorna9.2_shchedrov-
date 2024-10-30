b2343863c145422edab5083a30ef48ec7820fd84
Богдан Щедров <bogdan05ua@gmail.com>
2024-10-30 11:24:03 +0200

    Add project files.

---
+++ asplb9.2/Components/WeatherViewComponent.cs
@@ -0,0 +1,37 @@
+﻿namespace asplb9._2.Components
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+    using asplb9._2.Models;
+
+    public class WeatherViewComponent : ViewComponent
+    {
+        private readonly string apiKey = "";
+
+        public async Task<IViewComponentResult> InvokeAsync(string city)
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(url);
+                var data = JsonConvert.DeserializeObject<dynamic>(response);
+
+                var weatherInfo = new Weather
+                {
+                    Temperature = data.main.temp,
+                    Description = data.weather[0].description,
+                    Humidity = data.main.humidity,
+                    WindSpeed = data.wind.speed,
+                    City = data.name
+                };
+
+                return View("WeatherWidget", weatherInfo);
+            }
+        }
+    }
+
+
+}
---
+++ asplb9.2/Models/Weather.cs
@@ -0,0 +1,12 @@
+﻿namespace asplb9._2.Models
+{
+    public class Weather
+    {
+        public double Temperature { get; set; }
+        public string Description { get; set; }
+        public double Humidity { get; set; }
+        public double WindSpeed { get; set; }
+        public string City { get; set; }
+    }
+
+}
